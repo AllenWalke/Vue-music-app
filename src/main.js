899262c4454697ceaf9bb00989209a49a47b1658
alen <625303289@qq.com>
2018-01-30 15:38:32 +0800

    add

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import 'common/stylus/index.styl'
+import lazyLoad from 'vue-lazyload'
+fastclick.attach(document.body)
+Vue.use(lazyLoad,{
+  loading: require('common/image/default.png')
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
