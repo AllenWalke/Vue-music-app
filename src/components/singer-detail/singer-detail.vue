31d0c5c7c1ad8133df1864b62732fa12c435e8cc
alen <625303289@qq.com>
2018-02-08 10:17:22 +0800

    add

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -8,6 +8,7 @@
   import {mapGetters} from 'vuex'
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
   export default {
     computed: {
        ...mapGetters([
@@ -25,9 +26,20 @@
         }
         getSingerDetail(this.singer.id).then((res)=>{
           if(res.code===ERR_OK){
-
+            this.songs = this._normalizeSongs(res.data.list)
+            console.log(this.songs)
           }
         })
+      },
+      _normalizeSongs(list){
+        let ret = [];
+        list.forEach((item) => {
+          let {musicData} = item;
+          if(musicData.songid&&musicData.albummid){
+            ret.push(createSong(musicData))
+          }
+        });
+        return ret
       }
     }
   }
